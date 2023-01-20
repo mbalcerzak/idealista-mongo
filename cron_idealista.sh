686245254a0a552f9a3897249dc78c247fa6d1c9
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2023-01-20 20:21:47 +0100

    log with date

--- cron_idealista.sh
+++ cron_idealista.sh
@@ -2,6 +2,7 @@ cd /Users/malgorzatabslcerzak/Documents/projects/idealista-mongo/
 
 source .venv/bin/activate
 
-.venv/bin/python3.9 src/db_mongo.py &> log_inside
+now=$(date +"%m_%d_%Y")
+.venv/bin/python3.9 src/db_mongo.py > "log_inside_${now}"
 
 .venv/bin/python3.9 src/flat_info.py
