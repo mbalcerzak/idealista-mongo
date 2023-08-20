c1766606158b2211686c6804cfa7d2cd7dad66ee
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2023-08-20 17:27:03 +0200

    added size category to stats

--- src/flat_stats.py
+++ src/flat_stats.py
@@ -76,7 +76,24 @@ def get_flats_per_location() -> dict:
 
 
 def get_flats_per_area_cat() -> dict:
-    """Get flats per size of the flat (number of rooms)"""
+    """Get flats per size of the flat"""
+    db = get_db()
+    collection_flats = db["_flats"]
+
+    df = pd.DataFrame(list(collection_flats.find({})))
+
+    df = df[["propertyCode", "size"]]
+
+    df["area_cat"] = pd.cut(
+                        df["size"], 
+                        bins=[-1, 40, 60, 80, 100, 120, 140, 160, 999999],
+                        labels=["max 40m", "40-60m", "60-80", "80-100m", "100-120m","120-140m","140-160m", "160m+"])
+    
+    print(df.head())
+
+
+def get_flats_per_num_rooms() -> dict:
+    """Get flats per number of rooms in the flat"""
     db = get_db()
     collection_flats = db["_flats"]
 
@@ -181,14 +198,18 @@ def get_changes_count() -> dict:
 def get_changed_per_day_m_avg() -> dict:
     """Get moving average of number of flats price changes per day"""
 
+
 def get_scraped_per_month() -> dict:
     """Get number of flats scraped per day"""
+
 
 def get_posted_per_day() -> dict:
     """Get number of flats posted per day"""
 
+
 def get_posted_per_day_m_avg() -> dict:
     """Get moving average of number of flats posted per day"""
+
 
 def get_price_m_location() -> dict:
     """Get price per location per month"""
@@ -253,7 +274,13 @@ def get_price_m_loc_area_cat() -> dict:
         
     df_flats =  pd.DataFrame(list(flats))
 
-    flats = df_flats[['propertyCode', 'neighborhood', 'rooms', "size"]]
+    flats = df_flats[['propertyCode', 'neighborhood', "size"]]
+
+    flats["area_cat"] = pd.cut(
+                    flats["size"], 
+                    bins=[-1, 40, 60, 80, 100, 120, 140, 160, 999999],
+                    labels=["max 40m", "40-60m", "60-80", "80-100m", "100-120m","120-140m","140-160m", "160m+"])
+
 
     # ['_id', 'propertyCode', 'price', 'date']
     prices = collection_prices.find()
@@ -271,7 +298,7 @@ def get_price_m_loc_area_cat() -> dict:
     result["month"] = result["date"].apply(lambda x: x[:7])
     result["price_m2"] = result["price"]/result["size"]
 
-    grouped = result.groupby(['neighborhood', "month", "rooms"]).agg({'price_m2': ['mean', 'count']})
+    grouped = result.groupby(['neighborhood', "month", "area_cat"]).agg({'price_m2': ['mean', 'count']})
     grouped.columns = ['price_mean', 'price_count']
     grouped = grouped.reset_index()
     grouped["area_type"] = 'neighborhood'
@@ -281,16 +308,14 @@ def get_price_m_loc_area_cat() -> dict:
     for _, r in grouped.iterrows():
         neigh = r["neighborhood"]
         month = r["month"]
-        rooms = r["rooms"]
-        if rooms >= 7:
-            rooms = "7+"
+        area_cat = r["area_cat"]
         price = r["price_mean"]
         count = r["price_count"]
 
         results.append({
             "location":neigh,
             "month_num":month,
-            "area_category":str(rooms),
+            "area_category":area_cat,
             "avg_price_per_m":price, 
             "num_flats":count,
             "month":month})
@@ -321,26 +346,25 @@ def save_neighborhood_labels():
 
 
 if __name__ == "__main__":
-
     combined = {}
     
     combined["dates"] = get_dates()
-
     combined["flat_count"] = get_flat_count()
-    # combined["flats_per_location"] = get_flats_per_location()
-    # combined["flats_per_area_cat"] = get_flats_per_area_cat()
-    # combined["scraped_per_day"] = get_scraped_per_day()
-    # combined["changes_per_day"] = get_changes_per_day()
-    # combined["changes_per_flat"] = get_changes_per_flat()
-    # combined["changes_count"] = get_changes_count()
-    # combined["price_m_location"] = get_price_m_location()
-    # combined["posted_per_day"] = get_scraped_per_day()
-    # combined["price_m_loc_area_cat"] = get_price_m_loc_area_cat()
+    combined["flats_per_location"] = get_flats_per_location()
+    combined["flats_per_area_cat"] = get_flats_per_area_cat()
+    combined["flats_per_num_rooms"] = get_flats_per_num_rooms()
+    combined["scraped_per_day"] = get_scraped_per_day()
+    combined["changes_per_day"] = get_changes_per_day()
+    combined["changes_per_flat"] = get_changes_per_flat()
+    combined["changes_count"] = get_changes_count()
+    combined["price_m_location"] = get_price_m_location()
+    combined["posted_per_day"] = get_scraped_per_day()
+    combined["price_m_loc_area_cat"] = get_price_m_loc_area_cat()
 
-    # with open("output/flats_mabdata.json", "w") as f:
-    #     json.dump(combined, f)
+    with open("output/flats_mabdata.json", "w") as f:
+        json.dump(combined, f)
 
     # save_rooms_labels()
     # save_neighborhood_labels()
 
-    print(combined)
+    # print(combined)
