f84134d52f2eaf4b95c5d630bcee68bbbe0d27f2
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2024-05-19 22:47:59 +0200

    new data@

--- cron_idealista.sh
+++ cron_idealista.sh
@@ -6,7 +6,7 @@ now=$(date +"%m_%d_%Y")
 .venv/bin/python3.9 src/db_mongo.py --pages 20 &> "logs/log_inside_${now}"
 .venv/bin/python3.9 src/db_mongo.py --pages 3  --yolo_penthouse &> "logs/log_inside_${now}_penthouse"
 .venv/bin/python3.9 src/db_mongo.py --pages 4  --rent &> "logs/log_inside_${now}_rent"
-.venv/bin/python3.9 src/db_mongo.py --pages 2  --rent_penthouse &> "logs/log_inside_${now}_rent_penthouse"
+.venv/bin/python3.9 src/db_mongo.py --pages 4  --rent_penthouse &> "logs/log_inside_${now}_rent_penthouse"
 
 # .venv/bin/python3.9 src/penthouse.py
 .venv/bin/python3.9 src/flat_stats.py &> "logs/log_inside_${now}_price_stats"
